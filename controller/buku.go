f4e01cf11956d56f31182b1da6b3708034fecc50
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 07:58:31 +0700

    Add AddNewBook in controller🫸🌀✏️📗🐧🐳

--- controller/buku.go
+++ controller/buku.go
@@ -1,6 +1,11 @@
 package controller
 
-import "go-crud-book-api-postgres/models"
+import (
+	"encoding/json"
+	"go-crud-book-api-postgres/models"
+	"log"
+	"net/http"
+)
 
 type response struct {
 	ID      int64  `json:"id,omitempty"`
@@ -11,4 +16,23 @@ type Response struct {
 	Status  int           `json:"status"`
 	Message string        `json:"message"`
 	Data    []models.Book `json:"data"`
+}
+
+func AddNewBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+
+	err := json.NewDecoder(r.Body).Decode(&book)
+
+	if err != nil {
+		log.Fatalf("Cannot decode from request body.  %v", err)
+	}
+
+	insertID := models.AddNewBook(book)
+
+	res := response{
+		ID:      insertID,
+		Message: "Add New Book successfully!",
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
