3866fdaf5629e46a628db80845f321372c2e4536
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 07:56:59 +0700

    Add Response in controller🫸🌀✏️📗🐧🐳

--- controller/buku.go
+++ controller/buku.go
@@ -1,6 +1,14 @@
 package controller
 
+import "go-crud-book-api-postgres/models"
+
 type response struct {
 	ID      int64  `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+type Response struct {
+	Status  int           `json:"status"`
+	Message string        `json:"message"`
+	Data    []models.Book `json:"data"`
 }
