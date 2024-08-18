8cc51382a78f74481debfcc2476c1a3282787b57
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 07:42:31 +0700

    Add config.go🫸🌀✏️📗🐧🐳

---
+++ config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv" // package used to read the .env file
+	_ "github.com/lib/pq"      // postgres golang driver
+)
+
+func CreateConnection() *sql.DB {
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// check the connection
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to DB!")
+	// return the connection
+	return db
+}
+
+type NullString struct {
+	sql.NullString
+}
+
+func (s NullString) MarshalJSON() ([]byte, error) {
+	if !s.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(s.String)
+}
+
+func (s *NullString) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		s.String, s.Valid = "", false
+		return nil
+	}
+	s.String, s.Valid = string(data), true
+	return nil
+}
